bab0dc7be2d15efd60586147c684372daf7bfa6d
onsnov <114084570+onsnov@users.noreply.github.com>
2023-04-24 15:29:10 +0300

    add work 1,2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+  </a>
+       `;
+  })
+  .join('');
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/02-video.js
@@ -0,0 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeThrottled = throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}, 1000); 
+
+const time = player.on('timeupdate', timeThrottled);
+
+currentTimeFn();
+
+ function currentTimeFn()  {
+  const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  if (array) {
+    const currentTime = array['seconds'];
+    player.setCurrentTime(currentTime);
+  }
+}
